002660697e86e57c039387c99851644feec3726a
timelyportfolio <kent.russell@timelyportfolio.com>
2016-01-03 21:22:53 -0600

    add back shiny

--- R/d3kit_timeline.R
+++ R/d3kit_timeline.R
@@ -177,3 +177,32 @@ d3kit_timeline <- function(
     elementId = elementId
   )
 }
+
+
+#' Shiny bindings for d3kit_timeline
+#'
+#' Output and render functions for using d3kit_timeline within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a timeline
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name d3kit_timeline-shiny
+#'
+#' @export
+d3kit_timelineOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'd3kit-timeline', width, height, package = 'timelineR')
+}
+
+#' @rdname d3kit_timeline-shiny
+#' @export
+renderd3kit_timeline <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, d3kit_timelineOutput, env, quoted = TRUE)
+}
